2ea26b588eef163a1437671c80b78e7031e57175
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-13 11:42:23 +0000

    adding image to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -15,4 +15,7 @@ class Restaurant < ApplicationRecord
     reviews.average(:rating)
   end
 
+  has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
