007a62f9ffce6d382e4ec149e5800fc8c7e28566
ajbater <amanda22@hotmail.co.uk>
2017-01-13 10:59:09 +0000

    refactored using partials - new restaurant form

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -7,12 +7,6 @@ feature 'endorsing reviews' do
     leave_review(thoughts: "Amazing", rating: '5')
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
-    visit '/restaurants'
-    click_link 'Endorse Review'
-    expect(page).to have_content('1 endorsement')
-  end
-
   it 'a user can endorse a review, which increments the endorsement count', js: true do
     visit '/restaurants'
     click_link 'Endorse Review'
